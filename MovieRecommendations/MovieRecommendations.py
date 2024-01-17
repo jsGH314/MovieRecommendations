dabca2402713313cbb5ba1a86f37006add48ab39
JakeS <jakesnyder314@protonmail.com>
2024-01-17 15:30:02 -0600

    Can now generate reccomendations based on a selected favorite movie.

--- MovieRecommendations/MovieRecommendations.py
+++ MovieRecommendations/MovieRecommendations.py
@@ -1,21 +1,45 @@
 from abc import abstractproperty
-from movies import movies
+from movies import movies, AutocompleteFunctions
 from sorts import bubble_sort
+
 
 class MovieRecommendations:
     def __init__(self):
         self.favorites = []
+        self.reccomendations = []
         
     def greet(self):
         print("=====================================================")
         print("||   Welcome to the Movie Recommendation Engine!   ||")
         print("=====================================================")
     
-    def options(self):
+    def main_menu(self):
         print("=====================================================")
         print("                     MAIN MENU                       ")
         print("=====================================================")
         print("What would you like to do?")
+        print("1. Search all movies")
+        print("2. View favorites")
+        print("3. View all movies")
+        print("4. Exit")
+        print("=====================================================")
+        choice = input("Enter a number: ")      
+        if choice == "1":
+            return self.search()
+        elif choice == "2":
+            return self.view_favorites()
+        elif choice == "3":
+            return self.display_result(movies)
+        elif choice == "4":
+            print("Thank you for using the Movie Recommendation Engine!")
+            exit()
+            
+
+    def search(self, movie_list = movies):
+        print("=====================================================")
+        print("                     SEARCH                          ")
+        print("=====================================================")
+        print("How would you like to search?")
         print("1. Search by title")
         print("2. Search by actor")
         print("3. Search by director")
@@ -24,11 +48,27 @@ class MovieRecommendations:
         print("6. Search by director and genre")
         print("7. Search by actor and genre")
         print("8. Search by director, actor, and genre")
-        print("9. View favorites")
-        print("10. View all movies")
-        print("11. Exit")
+        print("9. Exit to main menu")
         print("=====================================================")
-        return input("Enter a number: ")
+        choice = input("Enter a number: ")
+        while choice != "9":
+            if choice == "1":
+                self.search_by_title(movie_list)
+            elif choice == "2":
+                self.search_by_actor(movie_list)
+            elif choice == "3":
+                self.search_by_director(movie_list)
+            elif choice == "4":               
+                self.search_by_genre(movie_list)
+            elif choice == "5":
+                self.search_by_director_and_actor(movie_list)
+            elif choice == "6":
+                self.search_by_director_and_genre(movie_list)
+            elif choice == "7":
+                self.search_by_actor_and_genre(movie_list)
+            elif choice == "8":
+                self.search_by_director_actor_and_genre(movie_list)
+        self.main_menu()       
     
     def display_result(self, result):
         count = 0
@@ -64,13 +104,19 @@ class MovieRecommendations:
                 print("2. No")
                 choice = input("Enter a number: ")
                 if choice == "1":
-                   if len(result) == 1:  
-                    movie_choice = input("Enter the number of the movie you would like to add: ")
-                    result = result[(int(movie_choice) - 1)]
-                    self.add_to_favorites(result)
-                    print("Movie added to favorites!")
+                    if len(result) == 1:
+                        result = result[0]
+                        self.add_to_favorites(result)
+                        print("Movie added to favorites!")
+                        self.main_menu()
+                    else:    
+                        movie_choice = input("Enter the number of the movie you would like to add: ")
+                        result = result[(int(movie_choice) - 1)]
+                        self.add_to_favorites(result)
+                        print("Movie added to favorites!")
+                        self.main_menu()
                 elif choice == "2":
-                    self.search()
+                    self.main_menu()
             elif sort_choice == "2":               
                 print("Would you like to add any of these movies to your favorites?")
                 print("1. Yes")
@@ -81,14 +127,28 @@ class MovieRecommendations:
                         result = result[0]
                         self.add_to_favorites(result)
                         print("Movie added to favorites!")
+                        self.main_menu()
                     else:    
                         movie_choice = input("Enter the number of the movie you would like to add: ")
                         result = result[(int(movie_choice) - 1)]
                         self.add_to_favorites(result)
                         print("Movie added to favorites!")
+                        self.main_menu()
                 elif choice == "2":
-                    self.search()
-    
+                    self.main_menu()
+        else:
+            print("Would you like to add this movie to your favorites?")
+            print("1. Yes")
+            print("2. No")
+            choice = input("Enter a number: ")
+            if choice == "1":
+                result = result[0]
+                self.add_to_favorites(result)
+                print("Movie added to favorites!")
+                self.main_menu()
+            elif choice == "2":
+                self.main_menu()
+            
     def add_to_favorites(self, result):
             self.favorites.append(result)
             
@@ -100,7 +160,9 @@ class MovieRecommendations:
             print("-----------------------------------------------------") 
             return self.search()
         else:
-            print("Your favorites:")
+            print("-----------------------------------------------------") 
+            print("                 Your favorites:                     ")
+            print("-----------------------------------------------------") 
             for dictionary in self.favorites:
                 print(str(index + 1) + ". " + dictionary['movie_title'])
                 print(f"Directed by {dictionary['director_name']}")
@@ -108,15 +170,23 @@ class MovieRecommendations:
                 print(f"Genres: {dictionary['genres']}")
                 print("-----------------------------------------------------")  
                 index += 1     
-        print("Would you like to sort your favorites?")
-        print("1. Yes")
-        print("2. No")
+        print("What would you like to do?")
+        print("1. Search Favorites")
+        print("2. Sort Favorites")
+        print("3. Generate Recommendations")
+        print("4. Return to main menu")
         choice = input("Enter a number: ")
         if choice == "1":
-            self.sort_result(self.favorites)
+            self.search(self.favorites)
         elif choice == "2":
-            self.search()
-        
+            self.sort_result(self.favorites)
+        elif choice == "3":
+            print("Which movie would you like to generate recommendations for?")
+            rec_choice = input("Enter a number: ")
+            selected_movie = self.favorites[(int(rec_choice) - 1)]
+            self.generate_recommendations(selected_movie)
+        elif choice == "4":
+            self.main_menu()
     
     def sort_result(self, result):
         print("How would you like to sort these movies?")
@@ -148,109 +218,121 @@ class MovieRecommendations:
             elif choice == "8":
                 result = bubble_sort(result, self.by_director_actor_and_genre)
             return result
-            #self.view_favorites()
-            #elif choice == "9":
+        self.main_menu()
 
-        self.search()
-            #return
-            
-        #self.view_favorites()
-        
-    def search(self):
-        choice = self.options()
-        while choice != "11":
+    def generate_recommendations(self, selected_movie):
+        print("How would you like to generate recommendations?")
+        print("1. By genre")
+        print("2. By actor")
+        print("3. By director")
+        print("4. Return to main menu")
+        choice = input("Enter a number: ")
+        while choice != "4":
+            print("-----------------------------------------------------")
+            print("                 Recommendations:                    ")
+            print("-----------------------------------------------------")
             if choice == "1":
-                self.search_by_title()
+                self.generate_recommendations_by_genre(selected_movie)
             elif choice == "2":
-                self.search_by_actor()
+                self.generate_recommendations_by_actor(selected_movie)
             elif choice == "3":
-                self.search_by_director()
-            elif choice == "4":               
-                self.search_by_genre()
-            elif choice == "5":
-                self.search_by_director_and_actor()
-            elif choice == "6":
-                self.search_by_director_and_genre()
-            elif choice == "7":
-                self.search_by_actor_and_genre()
-            elif choice == "8":
-                self.search_by_director_actor_and_genre()
-            elif choice == "9":
-                self.view_favorites()
-            elif choice == "10":
-                self.display_result(movies)
-        print("Thank you for using the Movie Recommendation Engine!")
-        exit()
-        
-        
-    #SEARCH FUNCTIONS
-    def search_by_title(self):
+                self.generate_recommendations_by_director(selected_movie)
+        self.main_menu()
+
+    def generate_recommendations_by_genre(self, selected_movie):
         result = []
-        title = input("Enter a title: ")
+        genres = selected_movie['genres_seperate']
         for movie in movies:
-            if movie['movie_title_lower'] == title.lower():
+            if any(genre in movie['genres_seperate'] for genre in genres):
                 result.append(movie)
         self.display_result(result)
-    
-    def search_by_actor(self):
+        
+    def generate_recommendations_by_actor(self, selected_movie):
         result = []
-        actor = input("Enter an actor: ")
+        actor = selected_movie['actor_1_name'].lower()
         for movie in movies:
-            if movie['actor_1_lower'] == actor.lower() or movie['actor_2_lower'] == actor.lower() or movie['actor_3_lower'] == actor.lower():
+            if movie['actor_1_lower'].startswith(actor.lower()) or movie['actor_2_lower'].startswith(actor.lower()) or movie['actor_3_lower'].startswith(actor.lower()):
+                result.append(movie)
+        self.display_result(result)
+        
+    def generate_recommendations_by_director(self, selected_movie):
+        result = []
+        director = selected_movie['director_name'].lower()
+        for movie in movies:
+            if movie['director_lower'].startswith(director.lower()):
+                result.append(movie)
+        self.display_result(result)
+                                      
+    #SEARCH FUNCTIONS
+    def search_by_title(self, movie_list):
+        result = []
+        title = input("Enter a title (full or first part): ")
+        for movie in movie_list:
+            if movie['movie_title_lower'].startswith(title.lower()):
+                result.append(movie)
+        self.display_result(result)
+        
+    
+    def search_by_actor(self, movie_list):
+        result = []
+        actor = input("Enter an actor's name (full or first part): ")
+        for movie in movie_list:
+            if movie['actor_1_lower'].startswith(actor.lower()) or movie['actor_2_lower'].startswith(actor.lower()) or movie['actor_3_lower'].startswith(actor.lower()):
                 result.append(movie)
         self.display_result(result)
 
-    def search_by_director(self):
+    def search_by_director(self, movie_list):
         result = []
-        director = input("Enter a director: ")
-        for movie in movies:
-            if movie['director_lower'] == director.lower():
+        director = input("Enter a director's name (full or first part): ")
+        for movie in movie_list:
+            if movie['director_lower'].startswith(director.lower()):
                 result.append(movie)
         self.display_result(result)
                 
-    def search_by_genre(self):
+    def search_by_genre(self, movie_list):
         result = []
-        genre = input("Enter a genre: ")
-        for movie in movies:
-            if genre.lower() in movie['genres_lower']:
+        genre = input("Enter a genre (full or first part): ")
+        for movie in movie_list:
+            if movie['genres_lower'].startswith(genre.lower()):
                 result.append(movie)
         #return result
         self.display_result(result)
+             
         
-    def search_by_director_and_actor(self):
+    def search_by_director_and_actor(self, movie_list):
         result = []
-        director = input("Enter a director: ")
-        actor = input("Enter an actor: ")
-        for movie in movies:
-            if movie['director_lower'] == director.lower() and (movie['actor_1_lower'] == actor.lower() or movie['actor_2_lower'] == actor.lower() or movie['actor_3_lower'] == actor.lower()):
+        director = input("Enter a director (full or first part): ")
+        actor = input("Enter an actor (full or first part): ")
+        for movie in movie_list:
+            if movie['director_lower'].startswith(director.lower()) and (movie['actor_1_lower'].startswith(actor.lower()) or movie['actor_2_lower'].startswith(actor.lower()) or movie['actor_3_lower'].startswith(actor.lower())):
                 result.append(movie)
         self.display_result(result)
         
-    def search_by_director_and_genre(self):
+    def search_by_director_and_genre(self, movie_list):
         result = []
-        director = input("Enter a director: ")
-        genre = input("Enter a genre: ")
-        for movie in movies:
-            if movie['director_lower'] == director.lower() and genre.lower() in movie['genres_lower']:
+        director = input("Enter a director (full or first part): ")
+        genre = input("Enter a genre (full or first part): ")
+        for movie in movie_list:
+            if movie['director_lower'].startswith(director.lower()) and movie['genres_lower'].startswith(genre.lower()):
                 result.append(movie)
         self.display_result(result)
     
-    def search_by_actor_and_genre(self):
+    def search_by_actor_and_genre(self, movie_list):
         result = []
-        actor = input("Enter an actor: ")
-        genre = input("Enter a genre: ")
-        for movie in movies:
-            if (movie['actor_1_lower'] == actor.lower() or movie['actor_2_lower'] == actor.lower() or movie['actor_3_lower'] == actor.lower()) and genre.lower() in movie['genres_lower']:
+        actor = input("Enter an actor (full or first part): ")
+        genre = input("Enter a genre (full or first part): ")
+        for movie in movie_list:
+            if (movie['actor_1_lower'].startswith(actor.lower()) or movie['actor_2_lower'].startswith(actor.lower()) or movie['actor_3_lower'].startswith(actor.lower())) and movie['genres_lower'].startswith(genre.lower()):
                 result.append(movie)
         self.display_result(result)
         
-    def search_by_director_actor_and_genre(self):
+    def search_by_director_actor_and_genre(self, movie_list):
         result = []
-        director = input("Enter a director: ")
-        actor = input("Enter an actor: ")
-        genre = input("Enter a genre: ")
-        for movie in movies:
-            if movie['director_lower'] == director.lower() and (movie['actor_1_lower'] == actor.lower() or movie['actor_2_lower'] == actor.lower() or movie['actor_3_lower'] == actor.lower()) and genre.lower() in movie['genres_lower']:
+        director = input("Enter a director (full or first part): ")
+        actor = input("Enter an actor (full or first part): ")
+        genre = input("Enter a genre (full or first part): ")
+        for movie in movie_list:
+            if movie['director_lower'].startswith(director.lower()) and (movie['actor_1_lower'].startswith(actor.lower()) or movie['actor_2_lower'].startswith(actor.lower()) or movie['actor_3_lower'].startswith(actor.lower())) and movie['genres_lower'].startswith(genre.lower()):
                 result.append(movie)
         self.display_result(result)
        
@@ -297,5 +379,5 @@ class MovieRecommendations:
 #RUN PROGRAM
 engine = MovieRecommendations()
 engine.greet()
-engine.search()
+engine.main_menu()
 
--- MovieRecommendations/movies.py
+++ MovieRecommendations/movies.py
@@ -3,12 +3,14 @@ import csv
 my_file = open('movie.csv', 'r')
 reader = csv.DictReader(my_file)
 movies = list()
+genres = list()
 for movie in reader:
     movie['director_lower'] = movie['director_name'].lower()
     movie['actor_1_lower'] = movie['actor_1_name'].lower()
     movie['actor_2_lower'] = movie['actor_2_name'].lower()
     movie['actor_3_lower'] = movie['actor_3_name'].lower()
     movie['genres_lower'] = movie['genres'].lower()
+    movie['genres_seperate'] = movie['genres'].lower().split(' ')
     movie['movie_title_lower'] = movie['movie_title'].lower()
     movies.append(movie)
- 
+         
